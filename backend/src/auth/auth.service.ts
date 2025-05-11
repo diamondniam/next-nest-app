df8e800a267aaa58cb52abca4d77a7e99b6fc1e8
diamondniam <1yungdiamond@gmail.com>
2025-05-11 15:47:16 +0400

    backend: prisma orm init

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  signin() {
+    return 'signin';
+  }
+
+  signup() {
+    return 'signup';
+  }
+
+  logout() {
+    return 'logout';
+  }
+}
